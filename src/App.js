140d492930a9f02db9d9f8a2071a4fc2467b487f
eurekaop4 <kewinmask3@hotmail.com>
2022-05-27 13:40:22 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+
+          <h1>Hello, World.</h1>
+          <h2>BankT</h2>
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
